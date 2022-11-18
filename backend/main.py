207cef9c33033c77a869debebd202ae5406bebdb
megharosejayan <megharose15@gmail.com>
2022-11-18 11:57:58 +0900

    auth updates with 0Auth2PasswordBearer

--- backend/main.py
+++ backend/main.py
@@ -8,11 +8,20 @@ from hashing import Hash
 from fastapi import FastAPI, HTTPException, Depends, Request,status
 from oauth import get_current_user
 from jwttoken import create_access_token
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
 from urllib.error import HTTPError
 
+from typing import Union
+
 import pymongo
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
 
 origins = [
     "http://localhost:3000",
@@ -30,19 +39,31 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["testdb"]
 mycol = mydb["testusers"]
 
-
 class User(BaseModel):
     username: str
-    company: str
-    password: str
+    email: str
+    password:str
+
+
 class Login(BaseModel):
     username: str
+    email:str
     password: str
 class Token(BaseModel):
     access_token: str
     token_type: str
 class TokenData(BaseModel):
     username: Optional[str] = None
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
 
 
 
@@ -68,3 +89,12 @@ def login(request:OAuth2PasswordRequestForm = Depends()):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     access_token = create_access_token(data={"sub": user["username"] })
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
+
+
+
